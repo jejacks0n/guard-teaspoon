87aa5a04a6b9ddc88598a461e7ccc9ce6278db84
Justin Chapman <commonmodestudio@gmail.com>
2014-05-16 17:30:20 -0400

    Refactor

--- lib/guard/teaspoon.rb
+++ lib/guard/teaspoon.rb
@@ -33,11 +33,11 @@ module Guard
       passed = @runner.run_all(@options[:run_all])
 
       if passed
-        ::Guard::Notifier.notify("Success", title: "Teaspoon Guard", image: :success)
+        notify(:success)
         reload
         true
       else
-        ::Guard::Notifier.notify("Failed", title: "Teaspoon Guard", image: :failed)
+        notify(:failed)
         @last_failed = true
         throw :task_has_failed
       end
@@ -58,15 +58,19 @@ module Guard
 
       if failed
         @last_failed = true
-        ::Guard::Notifier.notify("Failed", title: "Teaspoon Guard", image: :failed)
+        notify(:failed)
         throw :task_has_failed
       else
-        ::Guard::Notifier.notify("Success", title: "Teaspoon Guard", image: :success)
+        notify(:success)
         run_all if @last_failed
       end
     end
 
     private
+
+    def notify(status)
+      ::Guard::Notifier.notify(status.to_s.capitalize, title: "Teaspoon Guard", image: status)
+    end
 
     def remove_failed(paths)
       @failed_paths -= paths if @options[:keep_failed]
